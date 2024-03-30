592ddc87deaa6ff76aaf03f4b59d405269456935
Xpronoob <kevin_03_puris@hotmail.com>
2024-03-29 23:32:21 -0600

    Priest follow Player & Animations

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,21 +5,72 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    //set the values in the inspector
-    public Transform target; //drag and stop player object in the inspector
-    public float within_range;
-    public float speed;
+    //Behavior
+    [SerializeField]
+    public int patrol;
+    public float time;
+    public Animator animator;
+    public Quaternion angle;
+    public float grade;
+    public float radius = 8.0f;
+
+    public GameObject target;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void enemyBehavior()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > radius)
+        {
+            Debug.Log("Walking");
+
+            animator.SetBool("IsRunning", false);
+            time += 1 * Time.deltaTime;
+            if (time >= 4)
+            {
+                patrol = Random.Range(0, 2);
+                time = 0;
+            }
+            switch (patrol)
+            {
+                case 0:
+                    animator.SetBool("IsWalking", false);
+                    break;
+
+                case 1:
+                    grade = Random.Range(0, 360);
+                    angle = Quaternion.Euler(0, grade, 0);
+                    patrol++;
+                    break;
+
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5F);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    animator.SetBool("IsWalking", true);
+                    break;
+            }
+        }
+        else
+        {
+            Debug.Log("Running");
+
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+            animator.SetBool("IsWalking", false);
+
+            animator.SetBool("IsRunning", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
+    }
+
 
     public void Update()
     {
-        //get the distance between the player and enemy (this object)
-        float dist = Vector3.Distance(target.position, transform.position);
-        //check if it is within the range you set
-        if (dist <= within_range)
-        {
-            //move to target(player) 
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
-        }
-        //else, if it is not in rage, it will not follow player
+        enemyBehavior();
     }
 }
